5eac9ac2dae7f68c41b67b9de37c8149464713d2
Tafadzwa <tafadzwalnyamukapa@gmail.com>
2024-03-23 18:57:48 +0300

    Implemented try catch of error handling middleware

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,41 +1,55 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import { AccountTransactionInput } from "../interfaces/accountTransactions.interface";
 import { processTransaction } from "../services/wallet.service";
 import { TransactionType } from "../enums/transactionType.enum";
 
 export const withdrawRequestHandler = async (
   request: Request,
-  response: Response
+  response: Response,
+  next: NextFunction
 ) => {
   //Database calls call the service middleware
   const transaction: AccountTransactionInput =
     request.body as AccountTransactionInput;
   console.log("Account Transaction: " + JSON.stringify(transaction));
-
-  const proceesedTransaction = await processTransaction(
-    transaction.amount,
-    transaction.walletAccount,
-    TransactionType.DEBIT,
-    transaction.reference
-  );
-  console.log("Processed Transaction: " + JSON.stringify(proceesedTransaction));
-  response.send(proceesedTransaction);
+  try {
+    const proceesedTransaction = await processTransaction(
+      transaction.amount,
+      transaction.walletAccount,
+      TransactionType.DEBIT,
+      transaction.reference
+    );
+    console.log(
+      "Processed Transaction: " + JSON.stringify(proceesedTransaction)
+    );
+    response.send(proceesedTransaction);
+  } catch (error) {
+    console.log("Error processing transaction", error);
+    next(error);
+  }
 };
 
 export const depositRequestHandler = async (
   request: Request,
-  response: Response
+  response: Response,
+  next: NextFunction
 ) => {
   const transaction: AccountTransactionInput =
     request.body as AccountTransactionInput;
   console.log("Account Transaction: " + JSON.stringify(transaction));
-
-  const proceesedTransaction = await processTransaction(
-    transaction.amount,
-    transaction.walletAccount,
-    TransactionType.CREDIT,
-    transaction.reference
-  );
-  console.log("Processed Transaction: " + JSON.stringify(proceesedTransaction));
-  response.send(proceesedTransaction);
+  try {
+    const proceesedTransaction = await processTransaction(
+      transaction.amount,
+      transaction.walletAccount,
+      TransactionType.CREDIT,
+      transaction.reference
+    );
+    console.log(
+      "Processed Transaction: " + JSON.stringify(proceesedTransaction)
+    );
+    response.send(proceesedTransaction);
+  } catch (error) {
+    console.log("Error processing transaction", error);
+    next(error);
+  }
 };
