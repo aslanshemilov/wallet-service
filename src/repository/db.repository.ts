78cadc951b9ff607e669818745226feb7db91832
Tafadzwa <tafadzwalnyamukapa@gmail.com>
2024-03-24 07:37:41 +0300

    Added env variables on username and pass

--- src/repository/db.repository.ts
+++ src/repository/db.repository.ts
@@ -2,6 +2,6 @@ require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 module.exports = {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
-  //url: "mongodb+srv://fincra:fincra@cluster0.l2cm4pb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  //url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+  url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority&appName=Cluster0`,
 };
